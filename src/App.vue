e06546f3e0f6ee751d3067fac9fd1c9fd0ae2eca
neogen22 <nickomjotov@yandex.ru>
2024-03-03 12:55:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import CardVue from './components/CardVue.vue'
+export default {
+  components: {
+    CardVue
+  }
+}
+</script>
+
+<template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    rel="stylesheet"
+  />
+  <CardVue />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+}
+</style>
---
+++ src/components/CardVue.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="card">
+    <div class="temperature">{{ temperatureNow }}°</div>
+    <div class="city">Kondopoga</div>
+    <div class="bottom">
+      <div class="date">{{ weekday }}, {{ day }} {{ month }}</div>
+      <div class="flex">
+        <div class="weather">{{ weather }}</div>
+        <img :src="icon" />
+      </div>
+      <div class="temperature-max-and-min">{{ temperatureMin }}° / {{ temperatureMax }}°</div>
+    </div>
+  </div>
+</template>
+<script>
+import { DateTime } from 'luxon'
+export default {
+  data() {
+    return {
+      city: 'Kondopoga',      
+      day: DateTime.now().setLocale('en-GB').day,
+      icon: null,
+      month: DateTime.now().setLocale('en-GB').monthLong,
+      temperatureMax: null,
+      temperatureMin: null,
+      temperatureNow: null,
+      weather: null,
+      weekday: DateTime.now().setLocale('en-GB').weekdayLong
+    }
+  },
+  created() {
+    this.getWeather()
+  },
+  methods: {
+    async getWeather() {
+      const response = await fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=Kondopoga&units=metric&appid=91274b03e3834f51cd2d05561eefe477'
+      )
+      const data = await response.json()      
+      this.icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+      this.temperatureMax = Math.round(data.main.temp_max)
+      this.temperatureMin = Math.round(data.main.temp_min)
+      this.temperatureNow = Math.round(data.main.temp)
+      this.weather = data.weather[0].description
+    }
+  }
+}
+</script>
+<style>
+.bottom {
+  background-color: white;
+  border-radius: 0px 0px 16px 16px;
+  height: 93px;
+  margin-top: 165px;
+  padding-left: 16px;
+}
+.card {
+  background-color: black;
+  background-image: url('/public/K.jpg');
+  background-size: cover;
+  border-radius: 16px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.25);  
+  height: 332px;
+  width: 243px;
+}
+.city {
+  color: rgb(255, 255, 255);
+  font-family: 'Open Sans';
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: 1px;
+  padding-left: 25px;
+}
+.date {
+  color: rgb(25, 25, 25);
+  font-family: 'Open Sans';
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: 0px;  
+  padding-top: 16px;  
+}
+.flex {
+  display: flex;
+  flex-direction: row;
+  & img {
+    margin-left: 150px;
+    position: absolute;    
+  }
+}
+.temperature {
+  color: rgb(255, 255, 255);
+  font-family: 'Open Sans';
+  font-size: 40px;
+  font-weight: 600;
+  letter-spacing: -2%;
+  line-height: 54px;
+  padding-left: 36px;
+}
+.temperature-max-and-min {
+  color: rgb(107, 107, 107);
+  font-family: 'Open Sans';
+  font-size: 12px;
+  font-weight: 700;
+  letter-spacing: 0px;
+  line-height: 16px;
+  padding-top: 2px;  
+  text-align: left;
+}
+.weather {
+  color: rgb(107, 107, 107);
+  font-family: 'Open Sans';
+  font-size: 12px;
+  font-weight: 700;
+  letter-spacing: 0px;
+  line-height: 16px;
+  padding-top: 6px;
+  text-align: left;
+}
+</style>
